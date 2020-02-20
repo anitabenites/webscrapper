49628260eab3422c7d8a7d0f6bc9df62eada4e92
Ana Maria Benites Rodriguez <anitabenites2014@gmail.com>
2020-02-20 20:05:55 +0100

    Add Floki

--- lib/web_scrapper.ex
+++ lib/web_scrapper.ex
@@ -2,9 +2,17 @@ defmodule WebScrapper do
   def get_meetup(url) do
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{body: body, status_code: status_code}} ->
-        IO.puts body
+        body
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.puts reason
+        reason
     end
   end
+
+  def get_text(doc) do
+    {:ok, html} = Floki.parse_document(doc)
+    # IO.inspect(doc, label: "this is the doc")
+    Floki.find(html, ".exploreHome-categories-title")
+    |> Enum.filter(fn {html, _ , [text]} -> text == "Tech" end)
+  end
+
 end
